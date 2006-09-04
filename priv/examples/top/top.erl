452d2cdc27301a55f0fe7bc2ae38017c11f78e91
mats cronqvist <mats.cronqvist@gmail.com>
2006-09-04 11:42:22 +0000

    updated the 'top' (formerly known as simple) example.
    git-svn-id: https://gtknode.googlecode.com/svn/trunk@4 9999ab98-4a1b-0410-ba6a-d90168ca9a37

--- priv/examples/top/top.erl
+++ priv/examples/top/top.erl
@@ -12,8 +12,8 @@
 -import(filename,[join/1,dirname/1]).
 
 -record(st, {statusbar_ctxt,treeview}).
--record(treeview,{name,store,store_type,selection,cols=[]}).
--record(col,{title,attr,data_col,ctype,gval_name}).
+-record(treeview,{store,cols=[]}).
+-record(col,{title,attr,data_col,type}).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 start() -> 
@@ -26,39 +26,75 @@ stop() -> ?MODULE ! quit.
     
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 init() ->
+    %% start the c-node and it's port handler
     gtknode:start(?MODULE),
+
+    %% load the glade file into the c-node
     GladeFile = join([dirname(code:which(?MODULE)),?MODULE])++".glade",
     ssnd([],'GN_glade_init',[GladeFile]),
+
     loop(init_gui()).
 
 init_gui() ->
     treeview_init(state_init(#st{})).
 
 state_init(St) ->
+    %% init the status bar
     Id = ssnd(statusbar1,'Gtk_statusbar_get_context_id',["state"]),
     ssnd(statusbar1,'Gtk_statusbar_push',[Id,"connected"]),
     state_disc(St#st{statusbar_ctxt = Id}).
 
 treeview_init(St) ->
-    Cols = [#col{title="Proc",attr="text",data_col=0,ctype=gchararray},
-	    #col{title="Size",attr="text",data_col=1,ctype=gint},
-	    #col{title="Msgq",attr="text",data_col=2,ctype=gint},
-	    #col{title="Reds",attr="text",data_col=3,ctype=gint}],
-    St#st{treeview=treeview(treeview1, Cols)}.
+    %% the tree view columns
+    Cols = [#col{title="Proc",attr="text",data_col=0,type=string},
+	    #col{title="Size",attr="text",data_col=1,type=integer},
+	    #col{title="Msgq",attr="text",data_col=2,type=integer},
+	    #col{title="Reds",attr="text",data_col=3,type=integer}],
+    lists:foreach(fun(C) -> treeview_column(C) end, Cols),
+    
+    %% create the model (a list_store)
+    LS = ssnd([],'Gtk_list_store_newv',[length(Cols),[C#col.type||C<-Cols]]),
+
+    %% associate the model with the view
+    ssnd(treeview1,'Gtk_tree_view_set_model',[LS]),
+
+    St#st{treeview=#treeview{cols = Cols,
+			     store = LS}}.
+
+treeview_column(#col{title=Title,attr=Attr,data_col=Col}) ->
+    %% create a tree view column
+    TreeViewCol = ssnd([],'Gtk_tree_view_column_new',[]),
+    TextRend = ssnd([],'Gtk_cell_renderer_text_new',[]),
+    ssnd(TreeViewCol,'Gtk_tree_view_column_pack_start',[TextRend,false]),
+    ssnd(TreeViewCol,'Gtk_tree_view_column_set_title',[Title]),
+    ssnd(TreeViewCol,'Gtk_tree_view_column_add_attribute',[TextRend,Attr,Col]),
+    ssnd(TreeViewCol,'Gtk_tree_view_column_set_resizable',[true]),
+    ssnd(treeview1,'Gtk_tree_view_append_column',[TreeViewCol]).
+
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 loop(St) ->
     receive
-	{?MODULE,{signal,{window1,'GDK_DELETE'}}} -> quit();
-	{?MODULE,{signal,{quit1,'GDK_BUTTON_RELEASE'}}} -> quit();
-	{?MODULE,{signal,{connect,'GDK_BUTTON_RELEASE'}}} -> loop(conn(St));
-	{?MODULE,{signal,{disconnect,'GDK_BUTTON_RELEASE'}}} -> loop(disc(St));
-	{?MODULE,{signal,{about1,'GDK_BUTTON_RELEASE'}}} ->loop(show_about(St));
-	{?MODULE,{signal,{dialogb,'GDK_BUTTON_RELEASE'}}}->loop(hide_about(St));
-	{?MODULE,{signal,{dialog1,'GDK_DELETE'}}} ->       loop(hide_about(St));
-	{data,Data} -> loop(update(St,Data));
-	quit -> quit();
-	X -> io:fwrite("got ~p~n",[X]),loop(St)
+	%% user deleted top window
+	{?MODULE,{signal,{window1,_}}} 	  -> quit();
+	%% user selected quit menu item
+	{?MODULE,{signal,{quit1,_}}} 	  -> quit();
+	%% user selected  connect menu item
+	{?MODULE,{signal,{connect,_}}} 	  -> loop(conn(St));
+	%% user selected  disconnect menu item
+	{?MODULE,{signal,{disconnect,_}}} -> loop(disc(St));
+	%% user selected about menu item
+	{?MODULE,{signal,{about1,_}}} 	  -> loop(show_about(St));
+	%% user clicked ok in about dialog
+	{?MODULE,{signal,{dialogb,_}}}	  -> loop(hide_about(St));
+	%% user deleted about dialog
+	{?MODULE,{signal,{dialog1,_}}} 	  -> loop(hide_about(St));
+	%% we got data from the top_top process
+	{data,Data}                       -> loop(update(St,Data));
+	%% quit from the erlang shell
+	quit                              -> quit();
+	%% log other signals
+	X                                 -> io:fwrite("got ~p~n",[X]),loop(St)
     end.
 
 quit() -> gtknode:stop(?MODULE).
@@ -67,8 +103,10 @@ disc(St) -> do_disconnect(),state_disc(St).
 hide_about(St) -> ssnd(dialog1,'Gtk_widget_hide',[]),St.
 show_about(St) -> ssnd(dialog1,'Gtk_widget_show',[]),St.
 update(St,Data) -> 
+    ssnd(treeview1,'Gtk_widget_freeze_child_notify',[]),
     clear(St#st.treeview),
     populate(St#st.treeview,Data),
+    ssnd(treeview1,'Gtk_widget_thaw_child_notify',[]),
     St.
 state_disc(St) ->
     ssnd(statusbar1,'Gtk_statusbar_push',[St#st.statusbar_ctxt,"disconnected"]),
@@ -85,55 +123,20 @@ do_connect() -> top_top:assert(self()).
 do_disconnect() -> top_top:stop().
     
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-clear(#treeview{store_type=list,store=LS}) -> 
+clear(#treeview{store=LS}) -> 
     ssnd(LS,'Gtk_list_store_clear',[]).
 
 populate(_TV,[]) -> ok;
-populate(TV=#treeview{store_type=list,store=LS,cols=Cols},[RowData|Data]) ->
+populate(TV=#treeview{store=LS,cols=Cols},[RowData|Data]) ->
     ssnd(LS,'Gtk_list_store_append',[gtkTreeIter]),
     populate_list_row(LS,Cols,RowData),
     populate(TV,Data).
 
 populate_list_row(_LS,[],[]) -> ok;
 populate_list_row(LS,[Col|Cols],[Data|Datas]) ->
-    Dcol = Col#col.data_col,
-    ssnd(LS,'Gtk_list_store_set_value',[gtkTreeIter,Dcol,gval(Col,Data)]),
+    ssnd(gval,'GN_value_set',[Data]),
+    ssnd(LS,'Gtk_list_store_set_value',[gtkTreeIter,Col#col.data_col,gval]),
     populate_list_row(LS,Cols,Datas).
-
-gval(#col{ctype=gint,gval_name=Gval},Int) when integer(Int) ->
-    ssnd(Gval,'G_value_set_int',[Int]), Gval;
-gval(#col{ctype=gchararray,gval_name=Gval},Str) when list(Str) ->
-    ssnd(Gval,'G_value_set_string',[Str]), Gval.
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-treeview(Name, Cols) ->
-    lists:foreach(fun(C) -> treeview_column(Name,C) end, Cols),
-    {LS,NCols} = liststore(Name,Cols),
-    ssnd(Name,'Gtk_tree_view_set_model',[LS]),
-    #treeview{name = Name,
-	      cols = NCols,
-	      store_type = list,
-	      store = LS,
-	      selection = ssnd(Name,'Gtk_tree_view_get_selection',[])}.
-
-liststore(Name,Cols) -> 
-    {ssnd([],'Gtk_list_store_newv',[length(Cols),[C#col.ctype||C<-Cols]]),
-     [C#col{gval_name=make_gval(Name,C)} || C <- Cols]}.
-
-make_gval(Name,C) ->
-    Gval_name = list_to_atom(atom_to_list(Name)++
-			     integer_to_list(C#col.data_col)),
-    ssnd([],'GN_new_gvalue',[Gval_name,C#col.ctype]),
-    Gval_name.
-
-treeview_column(Treeview, #col{title=Title,attr=Attr,data_col=Col}) ->
-    TreeViewCol = ssnd([],'Gtk_tree_view_column_new',[]),
-    TextRend = ssnd([],'Gtk_cell_renderer_text_new',[]),
-    ssnd(TreeViewCol,'Gtk_tree_view_column_pack_start',[TextRend,false]),
-    ssnd(TreeViewCol,'Gtk_tree_view_column_set_title',[Title]),
-    ssnd(TreeViewCol,'Gtk_tree_view_column_add_attribute',[TextRend,Attr,Col]),
-    ssnd(TreeViewCol,'Gtk_tree_view_column_set_resizable',[true]),
-    ssnd(Treeview,'Gtk_tree_view_append_column',[TreeViewCol]).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 ssnd([],Cmd,Args) -> snd(Cmd,Args);
