1c5a4f98bca0ec4d6d5bd9cd3e6d8a942ed5e0dc
mats cronqvist <masse@klarna.com>
2015-10-08 16:44:54 +0200

    dumping autotools, wip

--- priv/generator/generate.sh
+++ priv/generator/generate.sh
@@ -1,12 +1,60 @@
-$(ERL) -noinput -run generator go "@GTKDOCLINKS@" \
-        "$(GTKVSN)" "$(STRUCTS)" \
-        g $(G_I) $(G_W_I) $(srcdir)/g_black.txt \
-        g_generated.h g_funcs.html \
-        g_crap_funcs.txt g_crap_types.txt \
-        gdk $(GDK_I) $(GDK_W_I) $(srcdir)/gdk_black.txt \
-        gdk_generated.h gdk_funcs.html \
-        gdk_crap_funcs.txt gdk_crap_types.txt \
-        gtk $(GTK_I) $(GTK_W_I) $(srcdir)/gtk_black.txt \
-        gtk_generated.h gtk_funcs.html \
-        gtk_crap_funcs.txt gtk_crap_types.txt \
-        -s erlang halt
+#!/bin/bash
+
+function incdir() {
+    BASE=gtk+-2.0
+    pkg-config --cflags $BASE | sed -n -e "s/^.*-I\([^ ]*include\/$1\).*$/\1/p"
+}
+
+function canonic() {
+    readlink -e $1
+}
+
+function gennifer() {
+    local PREFIX=$1
+    shift
+    local INCLUDES="$@"
+    local WHITELIST=$GEN_DIR/${PREFIX}_white.txt
+    local WHITES=$(echo $INCLUDES | tr " " "\n" | grep -f $WHITELIST)
+    cp $GEN_DIR/${PREFIX}_predefined.defs $TMP/${PREFIX}_generated.defs
+    python $GEN_DIR/h2def.py $INCLUDES >> $TMP/${PREFIX}_generated.defs
+    python $GEN_DIR/h2def.py $WHITES > $TMP/${PREFIX}_white_generated.defs
+}
+
+function genfiles() {
+    PREFIX=$1
+    echo \
+        $PREFIX \
+        $TMP/${PREFIX}_generated.defs \
+        $TMP/${PREFIX}_white_generated.defs \
+        $GEN_DIR/${PREFIX}_black.txt \
+        $TMP/${PREFIX}_generated.h \
+        $TMP/${PREFIX}_funcs.html \
+        $TMP/${PREFIX}_crap_funcs.txt \
+        $TMP/${PREFIX}_crap_types.txt
+}
+
+GEN_DIR=$(canonic $(dirname $BASH_SOURCE))
+C_SRC_DIR=$(canonic $GEN_DIR/../../c_src)
+TMP=$GEN_DIR/build
+GTK_VERSION=$(pkg-config --modversion gtk+-2.0)
+GTKDOCLINKS=no
+
+[ -d $TMP ] && rm -rf $TMP
+mkdir $TMP
+
+G_INCLUDES=$(echo $(incdir glib-2.0)/*/*.h)
+GTK_INCLUDES=$(echo $(incdir gtk-2.0)/gtk/*.h)
+GDK_INCLUDES=$(echo $(incdir gtk-2.0)/gdk/*.h)
+GDK_PIXBUF_INCLUDES=$(echo $(incdir gdk-pixbuf-2.0)/*/*.h)
+
+gennifer g   $G_INCLUDES
+gennifer gdk $GDK_INCLUDES $GDK_PIXBUF_INCLUDES
+gennifer gtk $GTK_INCLUDES
+
+erlc -o $TMP $GEN_DIR/*.erl
+
+erl -noinput -pa $TMP \
+    -run generator go \
+    $GTKDOCLINKS $GTK_VERSION $C_SRC_DIR/gtknode_structs.c \
+    $(genfiles g) $(genfiles gdk) $(genfiles gtk) \
+    -s erlang halt
