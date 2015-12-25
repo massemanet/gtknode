90dc8a91631a9e5ddbe1e98790ef3b082dbd9f1b
mats cronqvist <masse@cronqvi.st>
2015-12-25 14:42:43 +0100

    indent

--- priv/examples/hello/hello.erl
+++ priv/examples/hello/hello.erl
@@ -12,21 +12,21 @@
 -define(G(C,A),gtknode:cmd(hello,C,A)).
 
 start() ->
-    gtknode:start(hello),
-    Win = ?G('Gtk_window_new',['GTK_WINDOW_TOPLEVEL']),
-    But = ?G('Gtk_button_new_with_label',["butt"]),
-    ?G('Gtk_container_add',[Win,But]),
-    ?G('Gtk_widget_show',[Win]),
-    ?G('Gtk_widget_show',[But]),
-    ?G('GN_signal_connect',[But,clicked]),
-    ?G('GN_signal_connect',[Win,destroy]),
-    loop(Win,But).
+  gtknode:start(hello),
+  Win = ?G('Gtk_window_new',['GTK_WINDOW_TOPLEVEL']),
+  But = ?G('Gtk_button_new_with_label',["butt"]),
+  ?G('Gtk_container_add',[Win,But]),
+  ?G('Gtk_widget_show',[Win]),
+  ?G('Gtk_widget_show',[But]),
+  ?G('GN_signal_connect',[But,clicked]),
+  ?G('GN_signal_connect',[Win,destroy]),
+  loop(Win,But).
 
 loop(Win,But) ->
-    receive
-        {hello,{signal,{But,clicked}}} ->
-            io:fwrite("~p~n",[clicked]),
-            loop(Win,But);
-        {hello,{signal,{Win,destroy}}}  ->
-            gtknode:stop(hello)
-    end.
+  receive
+    {hello,{signal,{But,clicked}}} ->
+      io:fwrite("~p~n",[clicked]),
+      loop(Win,But);
+    {hello,{signal,{Win,destroy}}}  ->
+      gtknode:stop(hello)
+  end.
